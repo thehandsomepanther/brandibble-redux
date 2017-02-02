9ae8400fe721fe26f6240df874d39d985fb26357
keithpops <keith.poplawski@gmail.com>
2017-02-02 17:18:23 -0500

    adds status reducer node

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import data from './data';
 import session from './session';
+import status from './status';
 
 export default combineReducers({
   data,
   session,
+  status,
 });
---
+++ src/reducers/status.js
@@ -0,0 +1,51 @@
+import { Status } from 'utils/constants';
+import reduxCrud from 'redux-crud';
+
+const {
+  FULFILLED,
+  IDLE,
+  PENDING,
+  REJECTED,
+} = Status;
+
+const {
+  ALLERGENS_FETCH_START,
+  ALLERGENS_FETCH_SUCCESS,
+  ALLERGENS_FETCH_REJECTED,
+} = reduxCrud.actionTypesFor('allergens');
+
+const {
+  LOCATIONS_FETCH_START,
+  LOCATIONS_FETCH_SUCCESS,
+  LOCATIONS_FETCH_REJECTED,
+} = reduxCrud.actionTypesFor('locations');
+
+const {
+  MENUS_FETCH_START,
+  MENUS_FETCH_SUCCESS,
+  MENUS_FETCH_REJECTED,
+} = reduxCrud.actionTypesFor('menus');
+
+const initialState = {
+  fetchAllergens: IDLE,
+  fetchLocations: IDLE,
+  fetchMenu: IDLE,
+};
+
+export default function status(state=initialState, action) {
+  switch (action.type) {
+    case ALLERGENS_FETCH_START: return { ...state, fetchAllergens: PENDING }
+    case ALLERGENS_FETCH_SUCCESS: return { ...state, fetchAllergens: FULFILLED }
+    case ALLERGENS_FETCH_REJECTED: return { ...state, fetchAllergens: REJECTED }
+
+    case LOCATIONS_FETCH_START: return { ...state,   fetchLocations: PENDING }
+    case LOCATIONS_FETCH_SUCCESS: return { ...state, fetchLocations: FULFILLED }
+    case LOCATIONS_FETCH_REJECTED: return { ...state,  fetchLocations: REJECTED }
+
+    case MENUS_FETCH_START: return { ...state,   fetchMenu: PENDING }
+    case MENUS_FETCH_SUCCESS: return { ...state, fetchMenu: FULFILLED }
+    case MENUS_FETCH_REJECTED: return { ...state,  fetchMenu: REJECTED }
+
+    default: return state;
+  }
+}
