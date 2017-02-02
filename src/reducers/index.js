a15dc3943edb5182caff5152f28289fc221060a1
keithpops <keith.poplawski@gmail.com>
2017-02-02 18:54:06 -0500

    adds error node

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import data from './data';
+import error from './error';
 import session from './session';
 
 export default combineReducers({
   data,
+  error,
   session,
 });
