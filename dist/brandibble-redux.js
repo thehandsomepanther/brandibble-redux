3d7b26641edb91462e252f655a7304f16581b467
keithpops <keith.poplawski@gmail.com>
2017-02-02 18:58:28 -0500

    adds todo item

--- dist/brandibble-redux.js
+++ dist/brandibble-redux.js
@@ -41749,7 +41749,8 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; // TODO: reducer is untested
+
 
 exports.default = error;
 
