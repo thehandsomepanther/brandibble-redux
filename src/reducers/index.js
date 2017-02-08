7b0d88b148756f57337496ab6be52b13030d7677
Keith Pops <keith.poplawski@gmail.com>
2017-02-08 15:47:42 -0500

    adds status reducer (#16)
    * adds dependency for babel transform
    
    * adds constants
    
    * adds status reducer node
    
    * adds todo item
    
    * updates dist
    
    * fixes redux-crud syntax
    
    * adds validateUser status

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import data from './data';
 import session from './session';
+import status from './status';
 import user from './user';
 
 export default combineReducers({
   data,
   session,
+  status,
   user,
 });
---
+++ src/reducers/status.js
@@ -0,0 +1,58 @@
+// TODO: this reducer is untested
+import { VALIDATE_USER } from 'actions/session/user';
+import { Status } from 'utils/constants';
+import reduxCrud from 'redux-crud';
+
+const {
+  FULFILLED,
+  IDLE,
+  PENDING,
+  REJECTED,
+} = Status;
+
+const {
+  ALLERGENS_FETCH_START,
+  ALLERGENS_FETCH_SUCCESS,
+  ALLERGENS_FETCH_ERROR,
+} = reduxCrud.actionTypesFor('allergens');
+
+const {
+  LOCATIONS_FETCH_START,
+  LOCATIONS_FETCH_SUCCESS,
+  LOCATIONS_FETCH_ERROR,
+} = reduxCrud.actionTypesFor('locations');
+
+const {
+  MENUS_FETCH_START,
+  MENUS_FETCH_SUCCESS,
+  MENUS_FETCH_ERROR,
+} = reduxCrud.actionTypesFor('menus');
+
+const initialState = {
+  fetchAllergens: IDLE,
+  fetchLocations: IDLE,
+  fetchMenu: IDLE,
+  validateUser: IDLE,
+};
+
+export default function status(state=initialState, action) {
+  switch (action.type) {
+    case ALLERGENS_FETCH_START: return { ...state, fetchAllergens: PENDING }
+    case ALLERGENS_FETCH_SUCCESS: return { ...state, fetchAllergens: FULFILLED }
+    case ALLERGENS_FETCH_ERROR: return { ...state, fetchAllergens: REJECTED }
+
+    case LOCATIONS_FETCH_START: return { ...state,   fetchLocations: PENDING }
+    case LOCATIONS_FETCH_SUCCESS: return { ...state, fetchLocations: FULFILLED }
+    case LOCATIONS_FETCH_ERROR: return { ...state,  fetchLocations: REJECTED }
+
+    case MENUS_FETCH_START: return { ...state,   fetchMenu: PENDING }
+    case MENUS_FETCH_SUCCESS: return { ...state, fetchMenu: FULFILLED }
+    case MENUS_FETCH_ERROR: return { ...state,  fetchMenu: REJECTED }
+
+    case `${VALIDATE_USER}_START`: return { ...state,   validateUser: PENDING }
+    case `${VALIDATE_USER}_SUCCESS`: return { ...state, validateUser: FULFILLED }
+    case `${VALIDATE_USER}_ERROR`: return { ...state,  validateUser: REJECTED }
+
+    default: return state;
+  }
+}
