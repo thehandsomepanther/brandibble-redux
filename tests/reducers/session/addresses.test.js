9c2120f65e2360f6f3e3aaa0a97173f8c5354f6a
keithpops <keith.poplawski@gmail.com>
2017-02-14 17:47:48 -0500

    adds addresses reducers

---
+++ tests/reducers/session/addresses.test.js
@@ -0,0 +1,42 @@
+import { expect } from 'chai';
+import reduxCrud from 'redux-crud';
+import reducer from 'reducers/session/addresses';
+
+const {
+  ADDRESSES_FETCH_SUCCESS,
+  ADDRESSES_DELETE_SUCCESS,
+  ADDRESSES_CREATE_SUCCESS,
+} = reduxCrud.actionTypesFor('addresses');
+const initialState = {};
+
+describe('reducers/session/addresses', () => {
+  it('should return the initial state', () => {
+    expect(
+      reducer(initialState, {})
+    ).to.equal(initialState);
+  });
+
+  it('handles the ADDRESSES_FETCH_SUCCESS action', () => {
+    let reduced = reducer(initialState, {
+      type: ADDRESSES_FETCH_SUCCESS,
+      records: [{customer_address_id:1, street_address:'Dad Zone'}],
+    });
+    expect(reduced);
+  });
+
+  it('handles the ADDRESSES_DELETE_SUCCESS action', () => {
+    let reduced = reducer(initialState, {
+      type: ADDRESSES_DELETE_SUCCESS,
+      record: {customer_address_id:1, street_address:'Dad Zone'},
+    });
+    expect(reduced);
+  });
+
+  it('handles the ADDRESSES_CREATE_SUCCESS action', () => {
+    let reduced = reducer(initialState, {
+      type: ADDRESSES_CREATE_SUCCESS,
+      record: {customer_address_id:1, street_address:'Dad Zone'},
+    });
+    expect(reduced);
+  });
+});
