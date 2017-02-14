4320257174cfd268d2ec03cc863bb3212abd4a16
keithpops <keith.poplawski@gmail.com>
2017-02-14 13:51:57 -0500

    updates user attributes reducer

--- src/reducers/user/attributes.js
+++ src/reducers/user/attributes.js
@@ -1,8 +1,15 @@
+import reduxCrud from 'redux-crud';
 import {
   AUTHENTICATE_USER,
   RESOLVE_USER,
   UNAUTHENTICATE_USER,
 } from 'actions/session/user';
+
+const {
+  USER_CREATE_SUCCESS,
+  USER_FETCH_SUCCESS,
+  USER_UPDATE_SUCCESS,
+} = reduxCrud.actionTypesFor('user');
 const initialState = {};
 
 export default function attributes(state=initialState, action) {
@@ -10,6 +17,11 @@ export default function attributes(state=initialState, action) {
     case `${RESOLVE_USER}_FULFILLED`:
     case `${AUTHENTICATE_USER}_FULFILLED`:
       return action.payload;
+    case USER_FETCH_SUCCESS:
+    case USER_UPDATE_SUCCESS:
+      return action.record;
+    case USER_FETCH_SUCCESS:
+      return action.records;
     case `${UNAUTHENTICATE_USER}_FULFILLED`:
       return initialState;
     default:
