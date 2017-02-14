91216548c26c8e66368d9d8e76f5199d3ced8659
Keith Pops <keith.poplawski@gmail.com>
2017-02-13 20:09:56 -0500

    locations reducer / action updates (#18)
    * adds orderType as an argument
    
    * fixes redux-crud implementation
    
    * moves menu stub to stubs file
    
    * shortens findLocationByAddress return
    
    * adds locations reducer
    
    * updates dist
    
    * removes findLocationByAddress
    
    * adds limit to location function - TODO: add limit handling to brandibble.js
    
    * updates dist
    
    * moves lodash find to be a dev dependency
    
    * fixes merge conflict
    
    * updates build
    
    * fixes merge conflict
    
    * fixes locations action to accept query object
    
    * moves locations reducer to data node
    
    * bump version

--- tests/config/stubs.js
+++ tests/config/stubs.js
@@ -1,6 +1,7 @@
 import Brandibble from 'brandibble';
 import ENV from './environment';
 import localforage from 'localforage';
+import generateUUID from 'utils/generateUUID';
 
 localforage.config({name: 'brandibble-redux-test', storeName: 'brandibble-redux-test'});
 
@@ -94,3 +95,163 @@ export const authResponseStub = {
 
 export const SAMPLE_MENU_LOCATION_ID = 19;
 export const SAMPLE_EMAIL = 'sanctuary-testing-email@example.com';
+
+export const menusStub = {
+  id: generateUUID(),
+  expires_at: '2016-07-09T03:59:00Z',
+  sold_out_items: [0],
+  menu: [
+    {
+      slug: 'marketplates',
+      pos_display_color: '',
+      name: 'Marketplates',
+      items: [],
+    }
+  ],
+}
+
+export const locationsStub = [
+  {
+    'city':'Rye Brook',
+    'cross_streets':'The Rye Ridge Shopping Center',
+    'dayparts':[
+       {
+          'daypart':'Lunch',
+          'ends_at':'2017-01-26T20:00:00Z',
+          'has_delivery':false,
+          'has_pickup':true,
+          'starts_at':'2017-01-26T15:31:00Z',
+          'weekday':'Thursday'
+       },
+       {
+          'daypart':'Lunch',
+          'ends_at':'2017-01-29T20:00:00Z',
+          'has_delivery':false,
+          'has_pickup':true,
+          'starts_at':'2017-01-29T15:31:00Z',
+          'weekday':'Sunday'
+       }
+    ],
+    'delivery_minimum':0.0,
+    'delivery_zone':[
+       [
+          41.0032083,
+          -73.6833251
+       ],
+       [
+          41.0032124,
+          -73.6833037
+       ],
+       [
+          41.0032245,
+          -73.6833251
+       ],
+       [
+          41.0032083,
+          -73.6833251
+       ]
+    ],
+    'delivery_zone_description':'None',
+    'description':'',
+    'directions_url':'',
+    'distance':100,
+    'fax_number':null,
+    'has_delivery':false,
+    'has_pickup':true,
+    'hours_delivery':[
+
+    ],
+    'hours_description':'<p>Monday to Friday, 7:30am &mdash; 9pm<br />Saturday to Sunday, 8am &mdash; 9pm</p>',
+    'hours_for_week':[
+       {
+          'close':'9:00 PM',
+          'location_id':362,
+          'olo_close':'10:30 PM',
+          'olo_open':'7:30 AM',
+          'open':'7:30 AM',
+          'weekday':'Monday',
+          'weekday_int':0
+       },
+       {
+          'close':'9:00 PM',
+          'location_id':362,
+          'olo_close':'10:30 PM',
+          'olo_open':'7:30 AM',
+          'open':'7:30 AM',
+          'weekday':'Tuesday',
+          'weekday_int':1
+       },
+       {
+          'close':'9:00 PM',
+          'location_id':362,
+          'olo_close':'10:30 PM',
+          'olo_open':'7:30 AM',
+          'open':'7:30 AM',
+          'weekday':'Wednesday',
+          'weekday_int':2
+       },
+       {
+          'close':'9:00 PM',
+          'location_id':362,
+          'olo_close':'10:30 PM',
+          'olo_open':'7:30 AM',
+          'open':'7:30 AM',
+          'weekday':'Thursday',
+          'weekday_int':3
+       },
+       {
+          'close':'9:00 PM',
+          'location_id':362,
+          'olo_close':'10:30 PM',
+          'olo_open':'7:30 AM',
+          'open':'7:30 AM',
+          'weekday':'Friday',
+          'weekday_int':4
+       },
+       {
+          'close':'9:00 PM',
+          'location_id':362,
+          'olo_close':'10:30 PM',
+          'olo_open':'8:30 AM',
+          'open':'8:00 AM',
+          'weekday':'Saturday',
+          'weekday_int':5
+       },
+       {
+          'close':'9:00 PM',
+          'location_id':362,
+          'olo_close':'10:30 PM',
+          'olo_open':'8:30 AM',
+          'open':'8:00 AM',
+          'weekday':'Sunday',
+          'weekday_int':6
+       }
+    ],
+    'hours_pickup':[
+       '7:30 AM',
+       '9:00 PM'
+    ],
+    'hours_store':[
+       '7:30 AM',
+       '9:00 PM'
+    ],
+    'in_delivery_zone':false,
+    'is_coming_soon':false,
+    'is_new':true,
+    'is_open':true,
+    'latitude':41.0032095,
+    'location_id':362,
+    'longitude':-73.6833186,
+    'menu_pdf_url':'',
+    'name':'Rye Ridge',
+    'permanently_closed':false,
+    'phone_number':'914-305-8463',
+    'sales_tax':7.375,
+    'slug':'location-rye-ridge',
+    'state_code':'NY',
+    'street_address':'112 S. Ridge Street',
+    'temporarily_closed':false,
+    'timezone':'US/Eastern',
+    'zip_code':'10573'
+  }
+];
