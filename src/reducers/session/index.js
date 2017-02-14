95854667167c3901cb6f9ac05f0d37edfdfcbaa8
keithpops <keith.poplawski@gmail.com>
2017-02-14 18:55:37 -0500

    adds payments reducer

--- src/reducers/session/index.js
+++ src/reducers/session/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import menus from './menus';
 import order from './order';
+import payments from './payments';
 
 export default combineReducers({
   menus,
   order,
+  payments,
 });
