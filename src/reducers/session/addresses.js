1575c05ac1f7ecd0206f350f62820600f6060ce0
keithpops <keith.poplawski@gmail.com>
2017-02-14 17:47:48 -0500

    adds addresses reducers

---
+++ src/reducers/session/addresses.js
@@ -0,0 +1,11 @@
+import reduxCrud from 'redux-crud';
+const baseReducers = reduxCrud.Map.reducersFor('addresses', {key: 'customer_address_id'});
+
+const initialState = {};
+
+export default function addresses(state=initialState, action) {
+  switch(action.type) {
+    default:
+      return baseReducers(state, action);
+  }
+}
