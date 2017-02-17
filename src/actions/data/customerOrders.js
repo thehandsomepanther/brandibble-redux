3f177bf5c9fe62753dfbc24cbaef19ac55e666a5
sepowitz <sebastianodell@gmail.com>
2017-02-17 13:10:01 -0500

    changes status to query

--- src/actions/data/customerOrders.js
+++ src/actions/data/customerOrders.js
@@ -1,10 +1,10 @@
 import reduxCrud from 'redux-crud';
 const { fetchStart, fetchSuccess, fetchError } = reduxCrud.actionCreatorsFor('customerOrders');
 
-export function fetchCustomerOrders(brandibble, customerId, status={}) {
+export function fetchCustomerOrders(brandibble, customerId, query={}) {
   return dispatch => {
     dispatch(fetchStart());
-    return brandibble.customers.orders(customerId, status)
+    return brandibble.customers.orders(customerId, query)
       .then(({ data }) => dispatch(fetchSuccess(data, customerId)))
       .catch(({ errors }) => dispatch(fetchError(errors)));
   };
