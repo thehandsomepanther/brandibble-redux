fbee9eda6eb751790552b940f2096fab52ddfeb3
sepowitz <sebastianodell@gmail.com>
2017-02-24 11:01:32 -0500

    exports Status const

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import middleware from './config/middleware';
 import reducers from './reducers';
 import BrandibbleRef from 'brandibble';
 
+export * from './utils/constants';
 export * from './actions';
 export const Brandibble = BrandibbleRef;
 export const reducer = reducers;
