d2b524496a3046c36298ad9c3b97fa803b5908c5
Hugh Francis <me@hughfrancis.me>
2017-05-31 19:43:41 -0400

    Filter Unorderable Locations until JC implements filter

--- src/actions/data/locations.js
+++ src/actions/data/locations.js
@@ -1,4 +1,5 @@
 import reduxCrud from 'redux-crud';
+import filter from 'lodash.filter';
 
 const { fetchStart, fetchSuccess, fetchError } = reduxCrud.actionCreatorsFor('locations', { key: 'location_id' });
 
@@ -6,7 +7,16 @@ export function fetchLocations(brandibble, query = {}) {
   return (dispatch) => {
     dispatch(fetchStart());
     return brandibble.locations.index(query)
-      .then(({ data }) => dispatch(fetchSuccess(data)))
+      .then(({ data }) => {
+        // TODO: This is a temporary fix that should be taken out
+        // when JC properly implements the orderable flag.
+        // Pls Note: is_closed does not correspond to opening hours,
+        // it's related to temporary & permanently closed locations.
+        let orderableLocations = filter(data, location => {
+          return (!location.is_closed && !location.is_coming_soon);
+        });
+        dispatch(fetchSuccess(orderableLocations))
+      })
       .catch(response => {
         const { errors } = response;
         return dispatch(fetchError(errors || response));
