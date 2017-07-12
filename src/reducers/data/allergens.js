fe83a47c76cfcd9cac5610daa5eefa271cbde0e5
keithpops <keith.poplawski@gmail.com>
2017-07-12 14:36:15 -0400

    makes allergens immutable

--- src/reducers/data/allergens.js
+++ src/reducers/data/allergens.js
@@ -1,11 +1,18 @@
+import Immutable from 'seamless-immutable';
 import { FETCH_ALLERGENS } from 'actions/data/allergens';
 
-const initialState = [];
+export const initialState = Immutable({
+  allergensById: {},
+});
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case `${FETCH_ALLERGENS}_FULFILLED`:
-      return action.payload;
+      return state.merge({
+        allergensById: Immutable.asObject(action.payload, (allergen) => {
+          return [allergen.id, allergen];
+        }),
+      });
     default:
       return state;
   }
