3042e7a57bd98dab1defebe6db28df16ad02fba0
keithpops <keith.poplawski@gmail.com>
2017-07-12 12:52:15 -0400

    adds seamless-immutable to locations reducer

--- src/reducers/data/locations.js
+++ src/reducers/data/locations.js
@@ -1,21 +1,25 @@
+import Immutable from 'seamless-immutable';
 import {
   FETCH_LOCATION,
   FETCH_LOCATIONS,
 } from 'actions/data/locations';
 
-const initialState = [];
+export const initialState = Immutable({
+  locationsById: Immutable({}),
+});
 
 export default (state = initialState, action) => {
   const { payload, type } = action;
 
   switch (type) {
-    case `${FETCH_LOCATION}_FULFILLED`: {
-      const filtered = state.filter(loc => loc.id !== payload.id);
-      filtered.push(payload);
-      return filtered;
-    }
+    case `${FETCH_LOCATION}_FULFILLED`:
+      return state.setIn(['locationsById', payload.location_id], payload);
     case `${FETCH_LOCATIONS}_FULFILLED`:
-      return action.payload;
+      return state.merge({
+        locationsById: state.locationsById.merge(Immutable.asObject(payload, (location) => {
+          return [location.location_id, location];
+        })),
+      });
     default:
       return state;
   }
--- tests/reducers/data/locations.test.js
+++ tests/reducers/data/locations.test.js
@@ -1,13 +1,9 @@
 /* global describe it */
 import { expect } from 'chai';
-import {
-  FETCH_LOCATIONS,
-  FETCH_LOCATION,
-} from 'actions/data/locations';
-import reducer from 'reducers/data/locations';
+import { FETCH_LOCATIONS, FETCH_LOCATION } from 'actions/data/locations';
+import reducer, { initialState } from 'reducers/data/locations';
 import { locationsStub } from '../../config/stubs';
 
-const initialState = [];
 const payload = locationsStub;
 
 describe('reducers/data/locations', () => {
@@ -18,9 +14,9 @@ describe('reducers/data/locations', () => {
   it('handles the FETCH_LOCATION_FULFILLED action', () => {
     const reduced = reducer(initialState, {
       type: `${FETCH_LOCATION}_FULFILLED`,
-      payload,
+      payload: payload[0],
     });
-    expect(reduced[0].id).to.equal(payload[0].id);
+    expect(reduced.locationsById[payload[0].location_id]).to.exist;
   });
 
   it('handles the FETCH_LOCATIONS_FULFILLED action', () => {
@@ -28,6 +24,6 @@ describe('reducers/data/locations', () => {
       type: `${FETCH_LOCATIONS}_FULFILLED`,
       payload,
     });
-    expect(reduced[0].id).to.equal(payload[0].id);
+    expect(reduced.locationsById[payload[0].location_id]).to.exist;
   });
 });
