04809a798d1a3227248f349b50b6bad22f8afba2
keithpops <keith.poplawski@gmail.com>
2017-07-12 19:32:41 -0400

    updates locations actions / reducer

--- tests/reducers/data/locations.test.js
+++ tests/reducers/data/locations.test.js
@@ -1,11 +1,9 @@
 /* global describe it */
 import { expect } from 'chai';
-import reduxCrud from 'redux-crud';
-import reducer from 'reducers/data/locations';
+import { FETCH_LOCATIONS, FETCH_LOCATION } from 'actions/data/locations';
+import reducer, { initialState } from 'reducers/data/locations';
 import { locationsStub } from '../../config/stubs';
 
-const { LOCATIONS_FETCH_SUCCESS } = reduxCrud.actionTypesFor('locations');
-const initialState = [];
 const payload = locationsStub;
 
 describe('reducers/data/locations', () => {
@@ -13,11 +11,19 @@ describe('reducers/data/locations', () => {
     expect(reducer(initialState, {})).to.equal(initialState);
   });
 
-  it('handles the LOCATIONS_FETCH_SUCCESS action', () => {
+  it('handles the FETCH_LOCATION_FULFILLED action', () => {
     const reduced = reducer(initialState, {
-      type: LOCATIONS_FETCH_SUCCESS,
-      records: payload,
+      type: `${FETCH_LOCATION}_FULFILLED`,
+      payload: payload[0],
     });
-    expect(reduced[0].id).to.equal(payload[0].id);
+    expect(reduced.locationsById[payload[0].location_id]).to.exist;
+  });
+
+  it('handles the FETCH_LOCATIONS_FULFILLED action', () => {
+    const reduced = reducer(initialState, {
+      type: `${FETCH_LOCATIONS}_FULFILLED`,
+      payload,
+    });
+    expect(reduced.locationsById[payload[0].location_id]).to.exist;
   });
 });
