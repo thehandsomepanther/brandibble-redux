23f1aba938a6637315d7edd385b14870a4eb0b3f
Hugh Francis <me@hughfrancis.me>
2017-08-08 13:30:32 -0400

    Update Order Tests for new style

--- tests/actions/session/order.test.js
+++ tests/actions/session/order.test.js
@@ -132,8 +132,7 @@ describe('actions/session/order', () => {
       store = mockStore();
       const order = makeUnpersistedOrder();
 
-      return fetchMenu(brandibble, { locationId: SAMPLE_MENU_LOCATION_ID })(store.dispatch).then((response) => {
-        console.log(response, 'res')
+      return fetchMenu(brandibble, { locationId: SAMPLE_MENU_LOCATION_ID })(store.dispatch).then(({ value: { menu }}) => {
         const product = menu[0].children[menu[0].children.length - 1].items[0];
         order.cart.addLineItem(product, 1, product.id);
 
@@ -168,7 +167,7 @@ describe('actions/session/order', () => {
       store = mockStore();
       const order = makeUnpersistedOrder();
 
-      return fetchMenu(brandibble, SAMPLE_MENU_LOCATION_ID)(store.dispatch).then(({ menu }) => {
+      return fetchMenu(brandibble, { locationId: SAMPLE_MENU_LOCATION_ID })(store.dispatch).then(({ value: { menu }}) => {
         const product = menu[0].children[menu[0].children.length - 1].items[0];
         order.cart.addLineItem(product, 1, product.id);
 
@@ -464,7 +463,7 @@ describe('actions/session/order', () => {
       store = mockStore();
       const order = makeUnpersistedOrder();
 
-      return fetchMenu(brandibble, SAMPLE_MENU_LOCATION_ID)(store.dispatch).then(({ menu }) => {
+      return fetchMenu(brandibble, { locationId: SAMPLE_MENU_LOCATION_ID })(store.dispatch).then(({ value: { menu }}) => {
         const product = menu[0].children[menu[0].children.length - 1].items[0];
         order.cart.addLineItem(product, 1, product.id);
 
