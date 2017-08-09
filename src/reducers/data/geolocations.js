8acfd4c16767f80e21406af3481e09aefa1f7433
Hugh Francis <me@hughfrancis.me>
2017-08-09 14:05:15 -0400

    First pass at Geolocations

---
+++ src/reducers/data/geolocations.js
@@ -0,0 +1,15 @@
+import Immutable from 'seamless-immutable';
+import { FETCH_GEOLOCATIONS, CLEAR_GEOLOCATIONS } from 'actions/data/geolocations';
+
+export const initialState = Immutable([]);
+
+export default (state = initialState, action) => {
+  const { payload, type } = action;
+  switch (type) {
+    case `${FETCH_GEOLOCATIONS}_FULFILLED`:
+      return Immutable(payload)
+    case `${CLEAR_GEOLOCATIONS}_FULFILLED`:
+    default:
+      return state;
+  }
+};
