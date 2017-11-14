5dca051391cfcee6f0b5f26cd6174ced2bbc1625
Hugh Francis <hugh@sanctuary.computer>
2017-11-14 12:05:42 -0500

    Don’t Filter Unorderable Locations (#139)

--- src/selectors/locations.js
+++ src/selectors/locations.js
@@ -1,12 +1,21 @@
 import { createSelector } from 'reselect';
 import get from 'utils/get';
 import find from 'lodash.find';
+import filter from 'lodash.filter';
 
 export const locationsCollection = state => state.data.locations.locationsById;
 
 export const locationsAsArray = createSelector(
   locationsCollection,
   locations => Object.values(locations),
+);
+
+/* Note: "Closed" does not mean currently closed. It means permanently closed, etc */
+export const orderableLocationsAsArray = createSelector(
+  locationsCollection,
+  locations => filter(Object.values(locations), location => {
+    return (!location.is_closed && !location.is_coming_soon);
+  })
 );
 
 export const currentLocation = createSelector(
